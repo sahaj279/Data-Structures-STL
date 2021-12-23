7635873fe525ddb89d1ff0f4dd0fbf860a75f3d7
[sahaj shrivastava] <20bcs152@ietdavv.edu.in>
2021-12-23 23:12:25 +0530

    queue using array

---
+++ Queue.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Queue
+{
+public:
+    int *arr;
+    int front;
+    int last;
+    int size;
+    Queue()
+    {
+        arr = new int[10];
+        front = -1;
+        last = -1;
+        size = 10;
+    }
+    Queue(int n)
+    {
+        arr = new int[n];
+        front = -1;
+        last = -1;
+        size = n;
+    }
+    void enqueue(int a);
+    int dequeue();
+    void display();
+};
+
+void Queue::display()
+{
+    if (front == -1 || last + 1 == front)
+        return;
+    for (int i = front; i <= last; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+int Queue::dequeue()
+{
+    if (front == -1 || last + 1 == front)
+        return -1;
+    int a = front;
+    front++;
+    return arr[a];
+}
+void Queue::enqueue(int a)
+{
+    if (last == size - 1)
+    {
+        cout << "Queue overflow\n";
+        return;
+    }
+
+    if (front == -1)
+        front++;
+    last++;
+    arr[last] = a;
+}
+
+int main()
+{
+    // implementing queue using an array
+    Queue q(5);
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    q.enqueue(6);
+    q.display();
+    cout << q.dequeue();
+    cout << q.dequeue();
+    cout << q.dequeue();
+    cout << q.dequeue();
+    cout << q.dequeue();
+    cout << q.dequeue();
+
+    return 0;
+}
